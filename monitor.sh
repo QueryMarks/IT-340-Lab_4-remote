92d087b7ac713d03371b85e8adc12f71c9b1b326
Ethelyn <metallicminiboss@gmail.com>
2026-02-21 10:46:42 -0500

    Change to SYSTEM REPORT.

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Log the date and memory usage
 
-echo "Memory Log - $(date)" >> /home/ethelynux/Lab_4/system_log.txt
+echo "SYSTEM REPORT (Memory) - $date" >> /home/ethelynux/Lab_4/system_log.txt
 free -h | grep Mem >> /home/ethelynux/Lab_4/system_log.txt
 echo "--------------------------------" >> /home/ethelynux/Lab_4/system_log.txt
