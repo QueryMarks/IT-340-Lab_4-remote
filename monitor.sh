c1a8ce268e60652d83f5a3298a1b8a6ed7aca20b
Ethelyn <metallicminiboss@gmail.com>
2026-02-21 10:53:06 -0500

    Add DAILY MEMORY CHECK.

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Log the date and memory usage
 
-echo "Memory Log - $(date)" >> /home/ethelynux/Lab_4/system_log.txt
+echo "DAILY MEMORY CHECK - $(date)" >> /home/ethelynux/Lab_4/system_log.txt
 free -h | grep Mem >> /home/ethelynux/Lab_4/system_log.txt
 echo "--------------------------------" >> /home/ethelynux/Lab_4/system_log.txt
